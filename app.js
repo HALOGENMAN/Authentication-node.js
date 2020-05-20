c988edd69a8c5920061d690511ce7d231760e9de
HALOGENMAN <Shayak.malakar.159@gmail.com>
2020-05-20 12:35:24 +0530

    first

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 
 MONGOOSE_URI = "mongodb+srv://shayak:159951sm357753@cluster0-fidtq.mongodb.net/authentication" ;
 
+
 const app = express()
 
+app.set("view engine","ejs")
+app.set("views","views")
+
+app.use(bodyParser.urlencoded({extended:false}))
 app.use((req,res,next)=>{
     res.send("working")
 })
